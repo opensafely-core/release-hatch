f8af88da066c4f872e4f2953c0c22d83d998822f
bloodearnest <bloodearnest@gmail.com>
2021-08-02 17:19:54 +0100

    Hardcode the port in the docker entrypoint

--- docker/entrypoints/dev.sh
+++ docker/entrypoints/dev.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-port=$(echo "$SERVER_HOST" | awk -F: '{print $3}' | tr -d / )
-exec "$VIRTUAL_ENV/bin/uvicorn" hatch.app:app --reload --port "$port"
+exec "$VIRTUAL_ENV/bin/uvicorn" hatch.app:app --reload --port 8001
